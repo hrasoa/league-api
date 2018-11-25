1b0cf11d96bfd65324824ae229791644d474f7c9
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-11-25 19:20:01 +0100

    Add teams schema

--- src/teams/resolvers.js
+++ src/teams/resolvers.js
@@ -10,7 +10,7 @@ const teams = [
     logo: 'https://www.psdgold.com/timthumb.php?src=http://psdgold.com/uploaded_images/149270884886_fc_barcelona_logo_psd.png&h=300&w=300&q=100',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Paris Saint-Germain F.C',
     logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Paris_Saint-Germain_F.C..svg/1200px-Paris_Saint-Germain_F.C..svg.png',
   },

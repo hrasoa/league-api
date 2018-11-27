0c219a620d0d5b3c80378dbfb70bfbf6c9f4664d
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-11-27 18:38:38 +0100

    Update watch

--- src/dev.js
+++ src/dev.js
@@ -1,45 +1,43 @@
 const http = require('http');
 const path = require('path');
+const chokidar = require('chokidar'); // eslint-disable-line import/no-extraneous-dependencies
 const app = require('./app');
 
+let currentApp = app;
 const server = http.createServer(app);
 
 server.listen({ port: 4000 }, () => {
   console.log('🚀  Server ready');
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  const chokidar = require('chokidar'); // eslint-disable-line global-require,import/no-extraneous-dependencies
-  let currentApp = app;
+const watcher = chokidar.watch('.', {
+  ignored: path.basename(__filename),
+  cwd: path.resolve(__dirname),
+});
 
-  const watcher = chokidar.watch('.', {
-    ignored: path.basename(__filename),
-    cwd: path.resolve(__dirname),
-  });
+watcher.on('ready', () => {
+  console.log('- Watch ready');
+});
 
-  watcher.on('ready', () => {
-    console.log('- Watch ready');
-  });
-
-  watcher.on('change', (filePath) => {
-    console.log('- Changed ', filePath);
-    const moduleId = require.resolve(`./${filePath}`);
-    if (!require.cache[moduleId]) {
-      return;
-    }
-    clearCache(moduleId);
-    server.removeListener('request', currentApp);
-    const newApp = require('./app'); // eslint-disable-line global-require
-    server.on('request', newApp);
-    currentApp = newApp;
-  });
-}
+watcher.on('change', (filePath) => {
+  console.log('- Changed ', filePath);
+  const moduleId = require.resolve(`./${filePath}`);
+  if (!require.cache[moduleId]) {
+    return;
+  }
+  clearCache(moduleId);
+  server.removeListener('request', currentApp);
+  const newApp = require('./app'); // eslint-disable-line global-require
+  server.on('request', newApp);
+  currentApp = newApp;
+});
 
 function clearCache(moduleId) {
   const mod = { ...require.cache[moduleId] };
-  console.log('- Clear', moduleId);
   delete require.cache[moduleId];
-  if (mod.parent && mod.parent.id !== '.') {
+  if (mod.parent) {
+    console.log('- Clear', moduleId);
+    delete require.cache[moduleId];
     clearCache(mod.parent.id);
   }
 }

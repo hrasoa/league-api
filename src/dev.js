cc92cf98f95b7e32c7d493983ad63ed2ea3b6d94
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-11-27 18:46:38 +0100

    Update watch

--- src/dev.js
+++ src/dev.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const http = require('http');
 const path = require('path');
 const chokidar = require('chokidar'); // eslint-disable-line import/no-extraneous-dependencies

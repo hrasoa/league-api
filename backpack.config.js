8bbd774ff75a19ea94e032b5e90853a10c1809ab
Haja Rasoahaingo <hrasoahaingo@purch.com>
2019-01-13 14:53:15 +0100

    Update packages: use backpack

--- backpack.config.js
+++ backpack.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   webpack: config => {
-    // Perform customizations to config
-    // Important: return the modified config
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import app, { apolloServer } from './app';
+/* eslint-disable no-console */
+import express from 'express';
+import bodyParser from 'body-parser';
+import { ApolloServer } from 'apollo-server-express';
+import schema from './shema';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () => {
-  console.log(
-    `🚀  Server ready at http://localhost:4000${apolloServer.graphqlPath}`
-  ); // eslint-disable-line no-console
+  console.log(`🚀  Server ready at http://localhost:4000${server.graphqlPath}`);
 });
